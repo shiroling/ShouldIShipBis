637f7bdcf247f396b65584e920f783f885289451
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-03-29 09:44:57 +0200

    menus / liens entre activités

--- app/src/main/java/com/example/shouldishipbis/CompareActivity.java
+++ app/src/main/java/com/example/shouldishipbis/CompareActivity.java
@@ -1,9 +1,12 @@
 package com.example.shouldishipbis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CompareActivity extends AppCompatActivity {
@@ -18,8 +21,44 @@ public class CompareActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflaterMenu = getMenuInflater();
-        inflaterMenu.inflate(R.menu.menu_main, menu);
+        inflaterMenu.inflate(R.menu.menu_compare, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch(item.getItemId()) {
+            case R.id.menu_quitter:
+                System.exit(0);
+                break;
+            case R.id.menu_estimation:
+                goToEstimation();
+                break;
+            case R.id.menu_main:
+                goToMain();
+                break;
+            case R.id.menu_historique:
+                goToHistorique();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goToEstimation(){
+        Intent intent = new Intent(CompareActivity.this, FormActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMain(){
+        Intent intent = new Intent(CompareActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToHistorique(){
+        Intent intent = new Intent(CompareActivity.this, HistoricActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/shouldishipbis/HistoricActivity.java
+++ app/src/main/java/com/example/shouldishipbis/HistoricActivity.java
@@ -1,9 +1,12 @@
 package com.example.shouldishipbis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HistoricActivity extends AppCompatActivity {
@@ -18,8 +21,44 @@ public class HistoricActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflaterMenu = getMenuInflater();
-        inflaterMenu.inflate(R.menu.menu_main, menu);
+        inflaterMenu.inflate(R.menu.menu_historique, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch(item.getItemId()) {
+            case R.id.menu_quitter:
+                System.exit(0);
+                break;
+            case R.id.menu_estimation:
+                goToEstimation();
+                break;
+            case R.id.menu_comparer:
+                goToComparer();
+                break;
+            case R.id.menu_main:
+                goToMain();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goToEstimation(){
+        Intent intent = new Intent(HistoricActivity.this, FormActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToComparer(){
+        Intent intent = new Intent(HistoricActivity.this, CompareActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMain(){
+        Intent intent = new Intent(HistoricActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
